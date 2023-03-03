a8681525d1687ad81cfd6a68ea46ded679d4275a
devil <devilchinaqin@gmail.com>
2023-03-03 17:58:22 +0800

    feat: 地址

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     NODE_ENV: '"production"',
-    REACT_APP_BASE_URL: '"https://b2cweapp40673927e2a14ea49df338dc06bd4e9a.saas.qjclouds.com/"',
+    REACT_APP_BASE_URL: '"https://b2cweappbdc08f8461d04ec896a10818ac6c5f0d.saas.qjclouds.com/"',
     REACT_APP_SESSION_KEY: '"saas-token"',
     REACT_APP_APPLICATION: '""',
   },
