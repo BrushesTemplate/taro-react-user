c7f692a9f1173ca488fcb62c578541b0cc041401
Joker <dawn_gill@163.com>
2023-04-11 10:47:38 +0800

    feat: 修改收藏列表、公告详情

--- src/subpackage/noticedetail/index.tsx
+++ src/subpackage/noticedetail/index.tsx
@@ -1,31 +1,17 @@
-import { View, Image } from '@tarojs/components';
-import useNoticeDetail from "@/subpackage/noticedetail/useNoticeDetail";
 import {useRouter} from '@tarojs/taro';
+import {BaseWrapCommon} from "@/components/baseWrapCommon";
+import { View } from '@tarojs/components';
+import CommonJsx from '../../components';
+
 import './index.scss'
 
 const Index = () => {
-  const router = useRouter();
-  const {info} = useNoticeDetail(router.params.noticeId);
+  const { path = '/pages/index/index', params } = useRouter();
   return (
-    <View className='container'>
-      <View className='title'>
-        {info.noticeTitle}
-      </View>
-      {
-        info.noticePicurl?<Image className='img' mode='widthFix' src={info.noticePicurl} /> : null
-      }
-      {
-        info.noticePicurl1?<Image className='img' mode='widthFix' src={info.noticePicurl1} /> : null
-      }
-      {
-        info.noticePicurl2?<Image className='img' mode='widthFix' src={info.noticePicurl2} /> : null
-      }
-      {
-        info.noticePicurl3?<Image className='img' mode='widthFix' src={info.noticePicurl3} /> : null
-      }
-      <View className='content'>
-        {info.noticeContext}
-      </View>
+    <View className='container-goods-list'>
+      <BaseWrapCommon>
+        <CommonJsx route={path} {...params} />
+      </BaseWrapCommon>
     </View>
   )
 }
