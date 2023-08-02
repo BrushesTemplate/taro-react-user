306ac9e4288c41b4dcf9fc0dfa210ff06a6eab69
devil <devilchinaqin@gmail.com>
2023-08-02 16:21:12 +0800

    feat: package

--- config/dev.js
+++ config/dev.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     NODE_PLATFORM: '"MOBILE_TERMINAL"',
     NODE_ENV: '"development"',
-    REACT_APP_BASE_URL: '"https://b2cweappf1d20d00e904487eb5fc86ef50619f44.saas.qjclouds.com/"',
+    REACT_APP_BASE_URL: '"https://b2cweapp59ee5e8b678a4f2e92490a5a4332396e.saas.qjclouds.cn/"',
     WEAPP: '"https://b2clch5602e8be22bda48f68a550544e6ef42c1.saas.qjclouds.com/"',
     WEB: '"https://b2clch5cbaf0d729c1b4c7285b05271a454a4ff.saas.qjclouds.com/"',
     REACT_APP_SESSION_KEY: '"saas-token"',
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,12 +1,8 @@
 import {BaseWrapCommon} from "@/components/baseWrapCommon";
-import { useComponent } from '@brushes/simulate-component';
 
 const Index = () => {
-  const { View } = useComponent();
   return (
-    <View>
-      <BaseWrapCommon base />
-    </View>
+    <BaseWrapCommon base/>
   )
 }
 
